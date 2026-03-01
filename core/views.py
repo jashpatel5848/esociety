8fb6c67355b7681519c4a852d29be1dc9fd85292
jashpatel5848 <jashpatel5848@gmail.com>
2026-03-01 18:29:04 +0530

    Add welcome email template for new user registration

--- core/views.py
+++ core/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .forms import UserSignupForm,UserLoginForm 
 from django.contrib.auth import authenticate,login,logout
-from django.core.mail import send_mail
+from django.core.mail import send_mail,EmailMultiAlternatives
 from django.conf import settings
+from django.template.loader import render_to_string
+import os
 
 # Create your views here.
 def userSignupView(request): 
     if request.method == "POST":
         form = UserSignupForm(request.POST or None)
         if form.is_valid():
+           
+           
             #email send
             email = form.cleaned_data['email']
-            send_mail(subject="welcome to esociety",message="thank you for registering with e-society.",from_email=settings.EMAIL_HOST_USER,recipient_list=[email])
+            #send_mail(subject="welcome to esociety",message="thank you for registering with e-society.",from_email=settings.EMAIL_HOST_USER,recipient_list=[email])
+           
+            
+            # 🔹 HTML email
+            html_content = render_to_string(
+                'core/welcome_email.html',
+                {'email': email}
+            )
+            email_msg = EmailMultiAlternatives(
+                subject="Welcome to eSociety",
+                body="Thank you for registering with e-Society.",
+                from_email=settings.EMAIL_HOST_USER,
+                to=[email]
+            )
+            email_msg.attach_alternative(html_content, "text/html")
+
+            # 🔹 Image attachment (optional)
+            image_path = os.path.join(settings.BASE_DIR, 'static/images/welcome.png')
+            email_msg.attach_file(image_path)
+
+            email_msg.send()
+
+
+
             form.save()
             return redirect("login")
         else:  
